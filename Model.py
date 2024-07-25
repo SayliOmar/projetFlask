8e1b2cad85ea04c2b05e6d162bb6455ffe35551b
SayliOmar <ilyasomarhoussen@gmail.com>
2024-07-25 14:59:26 +0300

    le CRUD est en marche

--- Model.py
+++ Model.py
@@ -10,5 +10,6 @@ class User(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     email=db.Column(db.String(120), nullable=False)
     password=db.Column(db.String(10), nullable=False)
+    status = db.Column(db.String(10), nullable=False, default='user')
     
     
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, session, redirect ,url_for, flash
+from flask import Flask, render_template, request, session, redirect ,url_for, flash, session
+from flask_migrate import Migrate
 
 from Model import db , User
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Ecole.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Ecole1.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
 
@@ -12,6 +13,11 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+migrate=Migrate(app, db)
+
+
 
 @app.route('/')
 @app.route('/index')
@@ -25,9 +31,29 @@ def index():
 
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html',title='login' )
+    if request.method=="POST":
+        email=request.form['email']
+        password=request.form['password']
+        
+        # Récupérer l'utilisateur correspondant à l'email fourni
+        user = User.query.filter_by(email=email).first()
+        if user:
+            # Vérifier si le mot de passe correspond
+            if user.password== password:
+                flash('Vous êtes connecté!', 'success')
+                 # Stocker l'ID de l'utilisateur dans la session
+                session['user_id'] = user.id
+                return redirect(url_for('index'))
+            else:
+                flash('Mot de passe incorrect!', 'error')
+        else:
+           
+            flash('Utilisateur non trouvé!', 'error')       
+    return render_template('login.html', title='Login')
+    
+    
 
 
 @app.route('/inscription', methods=['GET', 'POST'])
@@ -36,12 +62,25 @@ def inscription():
         email=request.form['email']
         password=request.form['password']
         rePassword=request.form['rePassword']
-        newUser=User(email=email, password=password)
-        db.session.add(newUser)
-        db.session.commit()
-        return redirect(url_for('login'))
+        if password != rePassword:
+            flash('le deux mot de passe ne sont pas le meme', 'error')
+            return redirect(url_for('inscription'))
+        else:
+            
+            newUser=User(email=email, password=password)
+            db.session.add(newUser)
+            db.session.commit()
+            flash("Brovo! vous etes bien Inscrit", "success")
+            return redirect(url_for('login'))
     return render_template('inscription.html', title='inscription')
 
+@app.route('/sedeconnecter')
+def deconnecter():
+    
+    # Supprimer l'ID de l'utilisateur de la session
+    session.pop('user_id', None)
+    flash('Vous êtes déconnecté!', 'success')
+    return redirect(url_for('login'))
 
 @app.route('/formation')
 def formation():
@@ -52,6 +91,75 @@ def formation():
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title='contact')
+
+
+
+@app.route('/tableBoard', methods=['GET', 'POST'])
+def tableBoard():
+    users = User.query.all()
+    if request.method=='POST':
+        email=request.form['email']
+        password=request.form['password']
+        rePassword=request.form['rePassword']
+        if password != rePassword:
+            flash('le deux mot de passe ne sont pas le meme', 'error')
+            return redirect(url_for('tableBoard'))
+        else:
+            
+            newUser=User(email=email, password=password)
+            db.session.add(newUser)
+            db.session.commit()
+            flash("Brovo! vous etes bien Inscrit", "success")
+            return redirect(url_for('tableBoard'))
+    return render_template('tableauBord.html', title="table", users=users)
+
+
+
+
+# ajout d'un user
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    email = request.form['email']
+    password = request.form['password']
+    new_user = User(email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('tableBoard'))
+
+
+
+# suppresson d'un user
+
+@app.route('/delete_user/<int:user_id>', methods=['GET', 'POST'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('tableBoard'))
+
+
+
+
+
+
+# modfcaton d'un user
+
+@app.route('/edit_user/<int:user_id>', methods=['POST', 'GET'])
+def edit_user(user_id):
+    user = User.query.get(user_id)
+    if request.method == 'POST':
+        user.email = request.form['email']
+        user.password = request.form['password']
+        db.session.commit()
+        return redirect(url_for('tableBoard'))
+    else:
+        return render_template('modifier.html', user=user)
+
+
+
+    
 if __name__=="__main__":
     app.run(debug=True)
 
